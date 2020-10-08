24beb20bdd2e410ea5986a8f38444ce369ac0407
DanaZlotenko <danazlotenko80@gmail.com>
2020-10-08 16:34:52 +0300

    task 1

---
+++ 3.1.py
@@ -0,0 +1,14 @@
+name = input('Enter your name: ') #input
+surname = input('Enter your surname: ')
+number = input('Enter your phone number: ')
+street = input('Enter your street: ')
+house_n = input('Enter your house number: ')
+apartment = input('Enter your apartment number: ')
+city = input('Enter your city: ')
+index = input('Enter your index: ')
+country = input('Enter your country: ')
+print(name, surname) #output
+print(number)
+print('Str. ', street, ' ', house_n, ', ap. ', apartment, ', ', city)
+print(index)
+print(country)
